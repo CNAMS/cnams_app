8dcea1977b91673f9c56f1697e2eacfd0e9d4d1a
danish9661 <9661346164h@gmail.com>
2026-08-20 23:56:14 +0530

    Fix Android resource linking lStar error by setting compileSdkVersion 34 and updating printing

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.cgms_app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 34
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
